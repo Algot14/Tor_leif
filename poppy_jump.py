63880e314f8b2a038bc10f1cf887ba1e6573d32b
Sonja Colmsjö <sonjacolmsjo@Jonass-Air.lan>
2018-06-29 17:08:13 +0200

    Armhävning

--- poppy_jump.py
+++ poppy_jump.py
@@ -8,16 +8,32 @@ poppy = PoppyHumanoid(simulator='vrep')
 poppy.abs_y.goal_position = 40
 poppy.r_shoulder_y.goal_position = -90
 poppy.l_shoulder_y.goal_position = -90
-time.sleep(0.6)
+time.sleep(1)
 
 poppy.r_knee_y.goal_position = 90
 poppy.l_knee_y.goal_position = 90
 
-time.sleep(0.3)
+time.sleep(1)
 poppy.r_knee_y.goal_position = 0
 poppy.l_knee_y.goal_position = 0
 
+time.sleep(0.04)
+
+poppy.l_hip_x.goal_position = 20
+poppy.r_hip_x.goal_position = -20
+
+time.sleep(1)
+
+poppy.l_elbow_y.goal_position = -130
+poppy.r_elbow_y.goal_position = -130
+poppy.r_shoulder_y.goal_position = -10
+poppy.l_shoulder_y.goal_position = -10
 
 time.sleep(0.6)
 
-poppy.abs_y.goal_position = -0
+poppy.l_elbow_y.goal_position = 0.1
+poppy.r_elbow_y.goal_position = 0.1
+poppy.r_shoulder_y.goal_position = -90
+poppy.l_shoulder_y.goal_position = -90
+
+time.sleep(1)
